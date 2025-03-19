1a1e92b7c3f86c8d1090319cbc5ed59a3c03f7c4
Mickael Chau <mickaelchau@google.com>
2025-03-19 09:03:04 +0000

    with appropriate comments

--- terraform/secops-dac/main.tf
+++ terraform/secops-dac/main.tf
@@ -1,6 +1,13 @@
+terraform {
+  backend "gcs" {
+    bucket = "dac-demo-tfstate"
+    prefix = "terraform/state"
+  }
+}
+
 provider "google-beta" {
-  project = "micka-secops-lab"
-  region  = "us-central1"
+  project = "micka-secops-lab" # Put your Project ID
+  region  = "us-central1"      # Put the region you want to use by default => No impact for our example 
 }
 
 locals {
@@ -12,19 +19,20 @@ resource "google_chronicle_rule" "chronicle_rules" {
   provider = google-beta
   for_each = local.rules
   #rule_id         = each.key
-  location        = "Europe"
-  instance        = "57f24a5e-1eb5-4f76-9793-80bd3c4b7825"
+  location        = "Europe"                               # Put the location of your instance 
+  instance        = "57f24a5e-1eb5-4f76-9793-80bd3c4b7825" # Put Secops SIEM ID 
   deletion_policy = "FORCE"
   text            = each.value
 }
 
 resource "google_chronicle_rule_deployment" "chronicle_rules_deployment" {
-  provider      = "google-beta"
-  location      = "Europe"
-  instance      = "57f24a5e-1eb5-4f76-9793-80bd3c4b7825"
-  for_each      = local.rules
-  rule          = element(split("/", resource.google_chronicle_rule.chronicle_rules[each.key].name), 
-                  length(split("/", resource.google_chronicle_rule.chronicle_rules[each.key].name)) - 1)
+  provider = google-beta
+  location = "Europe"                               # Put the location of your instance 
+  instance = "57f24a5e-1eb5-4f76-9793-80bd3c4b7825" # Put Secops SIEM ID 
+  for_each = local.rules
+  rule = element(split("/", resource.google_chronicle_rule.chronicle_rules[each.key].name),
+  length(split("/", resource.google_chronicle_rule.chronicle_rules[each.key].name)) - 1)
+  # Get rule ID from google_chronicle_rule
   enabled       = true
   alerting      = true
   archived      = false
